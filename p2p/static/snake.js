d1dc0dffdb8898db1110e8cabaf173cffca9bc29
limenghua <menghua.li@netbraintech.com>
2017-07-25 14:16:52 +0800

    fix bug

--- p2p/static/snake.js
+++ p2p/static/snake.js
@@ -133,7 +133,7 @@ class SnakeGame {
     }
 
     step(directions){
-        processDirections(directions);
+        this.processDirections(directions);
 
         for (let i in this.snakes) {
             this.processOneSnake(this.snakes[i]);
